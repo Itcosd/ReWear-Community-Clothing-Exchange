8e25911fb17d2c56d491944c236318be4a0d2fb7
Maitrit1234 <maitrithakar1104@gmail.com>
2025-07-12 11:15:09 +0530

    feat: add User and Profile serializers for user data handling

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Profile
 # from .models import Student
 
 # class StudentSerializer(serializers.ModelSerializer):
 #     class Meta:
 #         model = Student
 #         fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
+        
+class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Profile
+        fields = ['id', 'user', 'points_balance']
